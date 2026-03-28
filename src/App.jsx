13f504fe83cff057afd3486780e95792aa8dfee8
Kenenisa Beyan <kenenisab05@gmail.com>
2026-03-28 15:11:20 +0300

    fix: correct tailwind syntax and responsive breakpoint spacing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <main>
       <div>
-        <h1>Welcome my Portifolio!</h1>
+        <h1 className="text-3xl underline">Welcome my Portifolio!</h1>
       </div>
     </main>
   )
